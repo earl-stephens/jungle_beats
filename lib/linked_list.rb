59b2a76ed63beb7a44d11eb31bb19cdc85d7dfb3
earl-stephens <stephensearl@hotmail.com>
2019-04-23 16:33:51 -0400

    refactored to better reflect the iteration 2 interaction pattern

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,20 +3,33 @@ require 'pry'
 class LinkedList
   attr_reader :head
 
-  def initialize(data)
-    @head = Node.new(data)
+  def initialize
+    @head = nil
   end
 
   def append(data)
-    last_node = @head
-    while last_node.next_node != nil
-      last_node = last_node.next_node
-    end
-    last_node.next_node = Node.new(data)
+    # last_node = @head
+    # unless last_node == nil
+    #   while last_node.next_node != nil
+    #     last_node = last_node.next_node
+    #   end
+    # end
+    # last_node = Node.new(data)
       # binding.pry
+      if @head == nil
+        @head = Node.new(data)
+      else
+        # binding.pry
+        last_node = @head
+        while last_node.next_node != nil
+          last_node = last_node.next_node
+        end
+        last_node.next_node = Node.new(data)
+      end
   end
 
   def count
+    # binding.pry
     if @head.next_node == nil
       return 1
     else
